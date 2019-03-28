8a35a4efc01bfdf8d7fcc3c79cde6ec99a588f01
chris <chrissalvado@hotmail.com>
2019-03-27 21:11:07 -0500

    chnaged the query url

--- liri.js
+++ liri.js
@@ -4,15 +4,15 @@ var keys = require("./keys.js");
 var spotify = require("node-spotify-api");
 var axios = require("axios");
 
-var QueryUrl = "http://www.omdbapi.com/?t=big&y=&plot=short&apikey=trilogy"
+var movieName = process.argv[2];
 
-var MovieName = process.argv[2]
+var QueryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
 
 axios.get(QueryUrl).then(
-    
+
     function(response) {
-        console.log("movie rating" + response.data.rating);
+        console.log(response.data);
 
         
 
